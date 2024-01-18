6f91f8500ab2c2477d58f3a60d03c5b939881bfb
xink <1611054604@qq.com>
2024-01-18 17:08:00 +0800

    fix:增加测试服

--- next.config.js
+++ next.config.js
@@ -47,8 +47,8 @@ module.exports = withLess(
         {
           source: '/api/:path*',
           // destination: 'http://45.159.209.53:8090/:path*', // 实际API的基本URL
-          // destination: 'http://194.36.171.87:8081/:path*', // 实际API的基本URL
-          destination: 'http://18.162.207.126:8081/:path*', // 实际API的基本URL
+          destination: 'http://194.36.171.87:8081/:path*', // 实际API的基本URL
+          // destination: 'http://18.162.207.126:8081/:path*', // 实际API的基本URL
           // destination: 'http://193.43.72.72:8081/:path*', // 实际API的基本URL
           // destination: 'http://192.168.1.16:8081/:path*', // 实际API的基本URL
           // destination: 'http://192.168.2.236:9292/:path*', // 实际API的基本URL
