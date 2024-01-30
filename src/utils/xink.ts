4e117d565f4da14d46cdf73ed392662ad116c48f
xink <1611054604@qq.com>
2024-01-30 23:28:54 +0800

    fix:还款查询完成

--- src/utils/xink.ts
+++ src/utils/xink.ts
@@ -1,6 +1,9 @@
 export const getQueryString = (name) => {
-  const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-  const r = window.location.search.substr(1).match(reg);
-  if (r !== null) return unescape(r[2]);
+  if (window === undefined) return null;
+  if(window){
+    const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    const r = window.location.search.substr(1).match(reg);
+    if (r !== null) return unescape(r[2]);
+  }
   return null;
 };
